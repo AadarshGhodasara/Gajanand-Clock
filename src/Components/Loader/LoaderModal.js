79ada6fe646a0cfd4e5852e9efd97351229e2f38
AadarshGhodasara <47548524+AadarshGhodasara@users.noreply.github.com>
2021-06-17 12:22:52 +0530

    Update LoaderModal.js

--- src/Components/Loader/LoaderModal.js
+++ src/Components/Loader/LoaderModal.js
@@ -4,7 +4,7 @@ import "./LoaderModal.css";
 
 const Modal = ({ text }) => {
   return (
-    <div className="loader-first-tag" style={{ zIndex: "1",backgroundColor: "transparent" }}>
+    <div className="loader-first-tag" style={{ zIndex: "1",backgroundColor: "#b3b3b3" }}>
       <div className="loader-sec-tag">
         <img src={LoaderLogo} alt="React Logo" />
         <label className="loader-font-text">
